b2a107e3faf08b1c7f014ae8a1f4d356d217cf8a
ahmadjaelani <ahmad.zaelan@gmail.com>
2022-11-22 14:39:55 +0700

    belajar react

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import './ExpenseItem.css'
+
+function Expenses(props){
+
+    return(
+        <Card className="expenses">
+        <ExpenseItem 
+          title={props.item[0].title}
+          amount={props.item[0].amount}
+          date={props.item[0].date}
+        ></ExpenseItem>
+        <ExpenseItem 
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        ></ExpenseItem>
+        <ExpenseItem 
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        ></ExpenseItem>
+        <ExpenseItem 
+          title={props.item[1].title}
+          amount={props.item[1].amount}
+          date={props.item[1].date}
+        ></ExpenseItem>
+      </Card>
+    );
+}
+export default Expenses;  
